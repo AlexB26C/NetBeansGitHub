6389b6f3e5be3391f43cef03b0a984a3edb53b61
AlexB26C <alex.barriga@educa.madrid.org>
2025-11-21 13:19:56 +0100

    Primer Commit Desde NetBeans

---
+++ NetBeansGitHub/src/netbeansgithub/Otra.java
@@ -0,0 +1,6 @@
+package netbeansgithub;
+public class Otra {
+    public static void main(String[] args) {
+        System.out.println("Adios");
+    }    
+}
---
+++ NetBeansGitHub/src/netbeansgithub/Saludo.java
@@ -0,0 +1,6 @@
+package netbeansgithub;
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("Hola  Mundo");
+    }
+}
